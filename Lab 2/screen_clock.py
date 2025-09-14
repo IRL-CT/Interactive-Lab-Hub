e4713f6d3738e2ff5aaef0ffac0a034635c029fe
Amanda Lu <azl8@cornell.edu>
2025-09-13 20:00:21 -0400

    basic clock function

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -66,6 +66,14 @@ while True:
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
 
+    print (time.strftime("%m/%d/%Y %H:%M:%S"), end="", flush=True)
+    print("\r", end="", flush=True)
+    
+    curr_time = time.strftime("%m/%d/%Y %H:%M:%S")
+
+    y = top
+    draw.text((x, y), curr_time, font=font, fill="#FFFFFF")
+
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
