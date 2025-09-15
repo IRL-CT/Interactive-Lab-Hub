40b2ae23b5689f67be4e188c051120879b8ef7b4
jaspreetl <jaspreetlal037@gmail.com>
2025-09-15 14:56:30 -0400

    add second image

--- Lab 2/image.py
+++ Lab 2/image.py
@@ -54,8 +54,37 @@ disp = st7789.ST7789(
     height=240,
     x_offset=53,
     y_offset=40,
+    rotation=90,
 )
 # pylint: enable=line-too-long
+
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output(value=True)
+
+buttonA = digitalio.DigitalInOut(board.D23)  # GPIO23
+buttonB = digitalio.DigitalInOut(board.D24)  # GPIO24
+buttonA.switch_to_input(pull=digitalio.Pull.UP)
+buttonB.switch_to_input(pull=digitalio.Pull.UP)
+
+
+def prep_image(path, width, height):
+    image = Image.open(path)
+
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    return image.crop((x, y, x + width, y + height))
+
 
 # Create blank image for drawing.
 # Make sure to create image with mode 'RGB' for full color.
@@ -65,37 +94,56 @@ if disp.rotation % 180 == 90:
 else:
     width = disp.width  # we swap height/width to rotate it to landscape!
     height = disp.height
-image = Image.new("RGB", (width, height))
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+image1 = prep_image("red.jpg", width, height)
+image2 = prep_image("quote.jpg", width, height)
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
-disp.image(image)
+print("Press A for red.jpg, B for blue.jpg, both for backlight OFF")
+while True:
+    a_pressed = (buttonA.value == False)
+    b_pressed = (buttonB.value == False)
 
-image = Image.open("red.jpg")
-backlight = digitalio.DigitalInOut(board.D22)
-backlight.switch_to_output()
-backlight.value = True
+    if a_pressed and b_pressed:
+        backlight.value = False
+    else:
+        backlight.value = True
+
+    if a_pressed and not b_pressed:
+        disp.image(image1)
+    elif b_pressed and not a_pressed:
+        disp.image(image2)
+
+    time.sleep(0.05)  # debounce
+
+# # Get drawing object to draw on image.
+# draw = ImageDraw.Draw(image)
+
+# # Draw a black filled box to clear the image.
+# draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+# disp.image(image)
+
+# image = Image.open("red.jpg")
+# backlight = digitalio.DigitalInOut(board.D22)
+# backlight.switch_to_output()
+# backlight.value = True
 
 
-# Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+# # Scale the image to the smaller screen dimension
+# image_ratio = image.width / image.height
+# screen_ratio = width / height
+# if screen_ratio < image_ratio:
+#     scaled_width = image.width * height // image.height
+#     scaled_height = height
+# else:
+#     scaled_width = width
+#     scaled_height = image.height * width // image.width
+# image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
+# # Crop and center the image
+# x = scaled_width // 2 - width // 2
+# y = scaled_height // 2 - height // 2
+# image = image.crop((x, y, x + width, y + height))
 
-# Display image.
-disp.image(image)
+# # Display image.
+# disp.image(image)
 
