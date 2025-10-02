6879faa35f7343e47c64bd847b91a0aaac92c0f6
Amanda Lu <azl8@cornell.edu>
2025-10-02 16:55:26 -0400

    create device file

--- Lab 4/camera_test.py
+++ Lab 4/camera_test.py
@@ -64,5 +64,5 @@ def test_speaker():
 
 if __name__ == "__main__":
     test_camera()
-    test_microphone()
-    test_speaker()
+    # test_microphone()
+    # test_speaker()
--- Lab 4/pi_servo_hat_test.py
+++ Lab 4/pi_servo_hat_test.py
@@ -3,7 +3,7 @@ import time
 
 # For most 9g micro servos (like SG90, MS18, SER0048), safe range is 0-120 degrees
 SERVO_MIN = 0
-SERVO_MAX = 120
+SERVO_MAX = 90
 SERVO_CH = 0  # Channel 0 by default
 
 servo = pi_servo_hat.PiServoHat()
@@ -17,12 +17,12 @@ try:
         for angle in range(SERVO_MIN, SERVO_MAX + 1, 1):
             servo.move_servo_position(SERVO_CH, angle)
             print(f"Angle: {angle}")
-            time.sleep(0.01)
+            time.sleep(0.001)
         # Sweep down
         for angle in range(SERVO_MAX, SERVO_MIN - 1, -1):
             servo.move_servo_position(SERVO_CH, angle)
             print(f"Angle: {angle}")
-            time.sleep(0.01)
+            time.sleep(0.001)
 except KeyboardInterrupt:
     print("\nTest stopped.")
     servo.move_servo_position(SERVO_CH, 60)  # Move to center on exit
