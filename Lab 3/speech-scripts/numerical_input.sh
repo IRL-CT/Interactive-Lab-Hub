89fb848c24f102a1fd6a5d96984aa9afa30596b8
ji227 <ji227@cornell.edu>
2025-10-03 20:44:42 -0400

    Update numerical_input.sh

--- Lab 3/speech-scripts/numerical_input.sh
+++ Lab 3/speech-scripts/numerical_input.sh
@@ -3,8 +3,7 @@
 # Use TTS to ask the question
 echo "Please state your 5-digit zip code now." | espeak
 
-# Record the response from your C270 webcam mic (hw:2,0) for 5 seconds
-# This creates a file named 'numerical_answer.wav'
+# Record the response 
 arecord -D hw:2,0 -f S16_LE -r 16000 -d 5 numerical_answer.wav
 
 echo "Recording complete. The audio is saved as numerical_answer.wav."
