98a0ae82834136a5b94dbd455b2c2353cdbb390a
js3888-shunshun <js3888@cornell.edu>
2025-12-02 20:21:11 -0500

    Update gesture_apds9960.py

--- Final Project/sensors/gesture_apds9960.py
+++ Final Project/sensors/gesture_apds9960.py
@@ -1,8 +1,20 @@
+# sensors/gesture_apds9960.py
+
 import board
 import busio
 from adafruit_apds9960.apds9960 import APDS9960
 
+
 class GestureSensor:
+    """
+    Wrapper for the APDS-9960 sensor.
+
+    - read_gesture(): returns a symbolic gesture string:
+        "expand" / "shrink" / "cooler" / "warmer" / None
+    - read_proximity(): returns a normalized proximity value in [0, 1]
+        where 0 is far away and 1 is very close to the sensor.
+    """
+
     def __init__(self):
         i2c = busio.I2C(board.SCL, board.SDA)
         self.sensor = APDS9960(i2c)
@@ -22,3 +34,17 @@ class GestureSensor:
             return "warmer"
         else:
             return None
+
+    def read_proximity(self):
+        """
+        Read proximity and map 0-255 to 0.0-1.0.
+        If the sensor fails, return None.
+        """
+        try:
+            raw = self.sensor.proximity  # 0~255
+        except Exception:
+            return None
+
+        # Normalize and clamp
+        value = max(0.0, min(1.0, raw / 255.0))
+        return value
